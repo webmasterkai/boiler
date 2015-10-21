fafc4e102ede07e06a23b5675ee7a120edfe9c3d
Kai Curry <kai@sundaysenergy.com>
2015-10-21 15:49:51 -0500

    is valid

--- src/components/WidgetForm/widgetValidation.js
+++ src/components/WidgetForm/widgetValidation.js
@@ -1,10 +1,10 @@
-import {createValidator, required, maxLength, integer, oneOf} from 'utils/validation';
+import {createValidator, isRequired, maxLength, isInteger, oneOf} from 'utils/validation';
 
 export const colors = ['Blue', 'Fuchsia', 'Green', 'Orange', 'Red', 'Taupe'];
 
 const widgetValidation = createValidator({
-  color: [required, oneOf(colors)],
-  sprocketCount: [required, integer],
-  owner: [required, maxLength(30)],
+  color: [isRequired, oneOf(colors)],
+  sprocketCount: [isRequired, isInteger],
+  owner: [isRequired, maxLength(30)],
 });
 export default widgetValidation;
